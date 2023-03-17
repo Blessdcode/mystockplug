63a495891ac8bd60783d9d3d31dbf1c88868c5d8
bnjmn <bmcode19@gmail.com>
2023-03-17 12:52:54 +0100

    first commit

---
+++ assets/main.js
@@ -0,0 +1,28 @@
+// const menuBtn = document.querySelector(".menu-btn");
+// const navLink = document.querySelector(".navlink");
+// const navLinks = document.querySelector(".nav-links");
+
+
+// menuBtn.addEventListener("click", () =>{
+//     navLink.classList.add('showMenu')
+// })
+
+// let showMenu = false
+
+// menuBtn.addEventListener('click', toggleMenu);
+
+// function toggleMenu() {
+//     if (!showMenu) {
+//         navLink.classList.add('showMenu');
+
+//         showMenu = true;
+//     } else {
+//         navLink.classList.remove('showMenu');
+//         showMenu = false
+//     }
+// }
+
+// window.addEventListener("scroll", function(){
+//     let nav = document.querySelector("nav");
+//     nav.classList.toggle("fixed-top", window.scrollY > 0);
+// })
